5def6f0506ec8e2f84a32def3a62dffd7e150f7c
Marcel Gehrung <marcel.gehrung@cruk.cam.ac.uk>
2020-06-28 13:11:22 +0100

    New code update

--- generate_mould.py
+++ generate_mould.py
@@ -1,22 +1,23 @@
 import argparse
+import math
 import os
 import subprocess
 import sys
+import time
 from ftplib import FTP
 
 import matplotlib.pyplot as plt
-import numpy as np
 import scipy.io as sio
-import stl
 from scipy import ndimage
 from scipy.spatial import ConvexHull
-from shapely.geometry import LineString, Polygon
 from skimage import measure
+
+import numpy as np
+import stl
+from shapely.geometry import LineString, Polygon
 from solid import *
 from solid.utils import *
 from stl import mesh
-import time
-import math
 
 # Parse all arguments from command line
 parser = argparse.ArgumentParser()
@@ -30,6 +31,7 @@ parser.add_argument("--tumourPoints", type=str, default="labels_smooth_box",
                     help="name of the volume points in the .mat input file")
 parser.add_argument('--gapBetweenSlabs', type=float, default=1.5)
 parser.add_argument('--sectionPlaneDistances', type=float, default=10)
+parser.add_argument('--slabHeight', type=float, default=47)
 parser.add_argument('--basePlateThickness', type=float, default=4)
 parser.add_argument('--printer', type=str, default='prusa-mk3s')
 # parser.add_argument("show_convex_hull",
@@ -52,7 +54,7 @@ def ftp_upload(ftp_obj, path, ftype='TXT'):
 
 
 def convert_scad_to_stl(scadFileName, stlFileName):
-    print("# Final conversion to .stl file:")
+    print("## Conversion to of " + scadFileName + ".scad to .stl file...")
     # Define in and out files
     in_file = scadFileName + ".scad"
     out_file = stlFileName + ".stl"
@@ -60,10 +62,31 @@ def convert_scad_to_stl(scadFileName, stlFileName):
     command = "openscad " + in_file + " -o " + out_file
 
     # Execute command
-    print("Going to execute: " + command)
-    output = subprocess.check_output(command, shell=True)
-    print("Done.")
+    #print("Going to execute: " + command)
+    output = subprocess.check_output(command, shell=True, stderr=open(outputFilePath + 'openscad.log', 'w'))
+    print("## done.")
     os.remove(in_file)
+
+
+def find_mins_maxs(obj):
+    minx = maxx = miny = maxy = minz = maxz = None
+    for p in obj.points:
+        # p contains (x, y, z)
+        if minx is None:
+            minx = p[stl.Dimension.X]
+            maxx = p[stl.Dimension.X]
+            miny = p[stl.Dimension.Y]
+            maxy = p[stl.Dimension.Y]
+            minz = p[stl.Dimension.Z]
+            maxz = p[stl.Dimension.Z]
+        else:
+            maxx = max(p[stl.Dimension.X], maxx)
+            minx = min(p[stl.Dimension.X], minx)
+            maxy = max(p[stl.Dimension.Y], maxy)
+            miny = min(p[stl.Dimension.Y], miny)
+            maxz = max(p[stl.Dimension.Z], maxz)
+            minz = min(p[stl.Dimension.Z], minz)
+    return minx, maxx, miny, maxy, minz, maxz
 
 
 # Assign all arguments to more readable variables
@@ -82,7 +105,7 @@ gapBetweenSlabs = args.gapBetweenSlabs  # Adapt to blade thickness
 sectionPlaneDistances = args.sectionPlaneDistances  # Default in our case
 basePlateThickness = args.basePlateThickness  # Thickness of mould base plate
 
-mouldSlabHeight = 47  # this
+mouldSlabHeight = args.slabHeight  # this
 contactHoleSize = 10  # size of holes in print
 
 sectionDirection = 0  # 0 = Along X?, 1 = Along Y?
@@ -99,8 +122,13 @@ if printerType == 'prusa-mk3s':
     sizeInXDirection = 210
     sizeInYDirection = 210
     sizeinZDirection = 250
+elif printerType == 'xl':
+    sizeInXDirection = 350
+    sizeInYDirection = 350
+    sizeinZDirection = 250
 else:
-    raise ValueError('Only Prusa i3 MK3s are supported in this version. Please modify the code to accomodate your printer limitations.')
+    raise ValueError(
+        'Only Prusa i3 MK3s are supported in this version. Please modify the code to accomodate your printer limitations.')
 
 
 annotatedSegmentationVolume = sio.loadmat(inputFilePath)
@@ -143,7 +171,8 @@ command += " -o '" + os.getcwd() + "/" + out_file + "' -om vn fn"
 # Execute command
 
 #print ("Going to execute: " + command)
-output = subprocess.check_output(command, shell=True, stderr=open(outputFilePath +'meshlab.log', 'w'))
+output = subprocess.check_output(
+    command, shell=True, stderr=open(outputFilePath + 'meshlab.log', 'w'))
 #last_line = output.splitlines()[-1]
 # print(last_line)
 # print("Done:")
@@ -158,18 +187,16 @@ testArray = np.array([processed_tumour.vectors[:, 0, 0],
 testArray.shape
 tumorHull = ConvexHull(testArray, incremental=True)
 
-#if args.show_convex_hull:
-    # plt.figure(figsize=(10, 10))
-    # plt.ylim([0, 210])
-    # plt.xlim([0, 210])
-    # plt.plot(processed_tumour.vectors[:, 0, 0],
-    #          processed_tumour.vectors[:, 0, 1])
-    # plt.plot(testArray[tumorHull.vertices, 0],
-    #          testArray[tumorHull.vertices, 1], 'r--', lw=2)
-    # plt.plot(testArray[tumorHull .vertices[0], 0],
-    #          testArray[tumorHull .vertices[0], 1], 'ro')
-    # plt.show()
-
+# plt.figure(figsize=(10, 10))
+# plt.ylim([0, 210])
+# plt.xlim([0, 210])
+# plt.plot(processed_tumour.vectors[:, 0, 0],
+#          processed_tumour.vectors[:, 0, 1])
+# plt.plot(testArray[tumorHull.vertices, 0],
+#          testArray[tumorHull.vertices, 1], 'r--', lw=2)
+# plt.plot(testArray[tumorHull .vertices[0], 0],
+#          testArray[tumorHull .vertices[0], 1], 'ro')
+# plt.show()
 
 convexHullArray = np.array([[testArray[a, 0], testArray[a, 1]]
                             for a in tumorHull.vertices])
@@ -183,7 +210,8 @@ mbr_lengths = [LineString(
 minor_axis = min(mbr_lengths)
 major_axis = max(mbr_lengths)
 
-print('# Minor axis: ' + str(round(minor_axis, 2)) + 'mm / Major axis: ' + str(round(major_axis, 2)) + 'mm')
+print('# Minor axis: ' + str(round(minor_axis, 2)) +
+      'mm / Major axis: ' + str(round(major_axis, 2)) + 'mm')
 print("# done.")
 
 print("# Build mould structure:")
@@ -204,10 +232,8 @@ for i in range(numberOfSections):
 d = intersection()(cube(
     [sizeInXDirection, sizeInYDirection, basePlateThickness]), convexHullExtrude2)
 d += intersection()(tmpSlabs, convexHullExtrude)
-d -= translate([0, 0, basePlateThickness])(import_stl(outputFilePath + "tumour_postproc.stl"))
-
-
-quit()
+d -= translate([0, 0, basePlateThickness]
+               )(import_stl(outputFilePath + "tumour_postproc.stl"))
 
 
 
@@ -239,12 +265,12 @@ d = difference()(d, translate([centerOfMassTumor[0], centerOfMassTumor[1],
 
 # Build a baseplate
 # Determind minimum and maximum extend in alldirections
-print(convexHullArray.shape)
-minX = np.min(convexHullArray[:, 0])
-maxX = np.max(convexHullArray[:, 0])
-minY = np.min(convexHullArray[:, 1])
-maxY = np.max(convexHullArray[:, 1])
-print(minX, maxX, minY, maxY)
+#print(convexHullArray.shape)
+#minX = np.min(convexHullArray[:, 0])
+#maxX = np.max(convexHullArray[:, 0])
+#minY = np.min(convexHullArray[:, 1])
+#maxY = np.max(convexHullArray[:, 1])
+#print(minX, maxX, minY, maxY)
 # print(sizeInXDirection/2-(maxX-minX)/2)
 #d = translate([sizeInYDirection/2-(maxY-minY)/2,sizeInXDirection/2-(maxX-minX)/2,0])(d)
 #d += translate([0,0,0])(cube([200,30,basePlateThickness]))
@@ -253,104 +279,67 @@ d = translate([barDepth, barDepth, 0])(d)
 d_tumour = translate([barDepth, barDepth, 0])(d_tumour)
 d_kidney = translate([barDepth, barDepth, 0])(d_kidney)
 
-scad_render_to_file(d, 'test.scad')
-scad_render_to_file(d_tumour, 'test_tumour.scad')
-scad_render_to_file(d_kidney, 'test_kidney.scad')
+scad_render_to_file(d, outputFilePath + 'tumour.scad')
+scad_render_to_file(d_tumour, outputFilePath + 'tumour_inlay.scad')
+scad_render_to_file(d_kidney, outputFilePath + 'kidney_inlay.scad')
 print("# done.")
 
+print("# Convert mould elements to final .stl files:")
+convert_scad_to_stl(outputFilePath + 'tumour', outputFilePath + 'mould_base')
+convert_scad_to_stl(outputFilePath + 'tumour_inlay', outputFilePath + 'mould_tumour_inlay')
+convert_scad_to_stl(outputFilePath + 'kidney_inlay', outputFilePath + 'mould_kidney_inlay')
+print("# done.")
 
 # find the max dimensions, so we can know the bounding box, getting the height,
 # width, length (because these are the step size)...
 
-
-def find_mins_maxs(obj):
-    minx = maxx = miny = maxy = minz = maxz = None
-    for p in obj.points:
-        # p contains (x, y, z)
-        if minx is None:
-            minx = p[stl.Dimension.X]
-            maxx = p[stl.Dimension.X]
-            miny = p[stl.Dimension.Y]
-            maxy = p[stl.Dimension.Y]
-            minz = p[stl.Dimension.Z]
-            maxz = p[stl.Dimension.Z]
-        else:
-            maxx = max(p[stl.Dimension.X], maxx)
-            minx = min(p[stl.Dimension.X], minx)
-            maxy = max(p[stl.Dimension.Y], maxy)
-            miny = min(p[stl.Dimension.Y], miny)
-            maxz = max(p[stl.Dimension.Z], maxz)
-            minz = min(p[stl.Dimension.Z], minz)
-    return minx, maxx, miny, maxy, minz, maxz
-
-
-main_body = mesh.Mesh.from_file('Patient.stl')
+main_body = mesh.Mesh.from_file(outputFilePath + 'mould_base.stl')
 
 minx, maxx, miny, maxy, minz, maxz = find_mins_maxs(main_body)
 
-# the logic is easy from there
-print("X:", maxx - minx)
-print("Y:", maxy - miny)
-print("Z:", maxz - minz)
-
 # Build cut guide ushape
-d_guide = translate([0, 0, 0])(
+d_guide = translate([0, maxy - 2, 0])(
     cube([(maxx - minx) + 2 * barDepth, barDepth, basePlateThickness]))
 d_guide += translate([0, barDepth, 0]
                      )(cube([barDepth, maxy - miny,  basePlateThickness]))
-d_guide += translate([maxx - minx + barDepth, barDepth, 0]
-                     )(cube([barDepth, maxy - miny,  basePlateThickness]))
+#d_guide += translate([maxx - minx + barDepth, barDepth, 0]
+#                     )(cube([barDepth, maxy - miny,  basePlateThickness]))
 
 numberOfSectionsGuide = math.ceil((
     maxy - miny) // sectionPlaneDistances if sectionDirection == 1 else (maxx - minx) // sectionPlaneDistances)
 for i in range(numberOfSectionsGuide):
     translateInX = i * sectionPlaneDistances if sectionDirection == 0 else 0
-    translateInY = i * sectionPlaneDistances if sectionDirection == 1 else 0
+    translateInY = i * sectionPlaneDistances if sectionDirection == 1 else maxy-2
     slabSize = [barDepth, sectionPlaneDistances - (gapBetweenSlabs / 2), columnHeight] if sectionDirection == 1 else [
         sectionPlaneDistances - (gapBetweenSlabs / 2), barDepth, columnHeight]
-    d_guide += translate([barDepth + translateInX, translateInY, basePlateThickness])(
+    d_guide += translate([barDepth + translateInX, translateInY, basePlateThickness] if sectionDirection == 0 else [translateInX, barDepth + translateInY, basePlateThickness])(
         cube(slabSize))
-    d_guide -= translate([barDepth + translateInX-4, translateInY+4, basePlateThickness + columnHeight-1])(
-        linear_extrude(3)(rotate([0,0,180])(text(str(i), font="Liberation Sans", size=3.5))))
-    d_guide -= translate([barDepth + translateInX, translateInY+6, basePlateThickness + columnHeight-1])(
-        cube([2,2,2]))
-    #d_guide += translate([maxx - minx + 1, barDepth + translateInY, basePlateThickness])(
+    # d_guide += translate([maxx - minx + 1, barDepth + translateInY, basePlateThickness])(
     #    cube(slabSize))
+for i in range(numberOfSectionsGuide+1):
+    translateInX = i * sectionPlaneDistances if sectionDirection == 0 else 0
+    translateInY = i * sectionPlaneDistances if sectionDirection == 1 else maxy-2
+    d_guide -= translate([barDepth + translateInX - 8, translateInY+4, basePlateThickness + columnHeight - 1] if sectionDirection == 0 else [barDepth + translateInX - 5, translateInY + 2, basePlateThickness + columnHeight - 1])(
+            linear_extrude(3)(rotate([0, 0, 90] if sectionDirection == 1 else [0, 0, 0])(text(str(i), font="Liberation Sans", size=3.5))))
+    d_guide -= translate([barDepth + translateInX - 10, translateInY+1, basePlateThickness + columnHeight - 1] if sectionDirection == 0 else [barDepth + translateInX - 4, translateInY, basePlateThickness + columnHeight - 1])(
+            cube([2, 2, 2]))
 
-scad_render_to_file(d_guide, 'test_guide.scad')
+scad_render_to_file(d_guide, outputFilePath + 'mould_guide.scad')
+scad_render_to_file(d + d_guide, outputFilePath + 'mould_combined_guide.scad')
 
-scad_render_to_file(d+d_guide, 'test_combined_guide.scad')
 
+print("# Convert mould elements to final .stl files:")
+convert_scad_to_stl(outputFilePath + 'mould_guide', outputFilePath + 'mould_guide')
+convert_scad_to_stl(outputFilePath + 'mould_combined_guide', outputFilePath + 'mould_combined_guide')
+print("# done.")
 
-filesToConvert = ['test.scad', 'test_tumour.scad',
-                  'test_kidney.scad', 'test_guide.scad','test_combined_guide.scad']
-targetFileNames = [outputFilePath, outputFilePath +
-                   "_tumour", outputFilePath + "_kidney", outputFilePath + "_guide", outputFilePath + "_combined_guide"]
-for fileIdx, fileToConvert in enumerate(filesToConvert):
-    print("# Final conversion to .stl file:")
-    # Add input mesh
-    in_file = fileToConvert
-    out_file = targetFileNames[fileIdx] + ".stl"
-    command = "openscad " + in_file + ""
-    # Add the output filename and output flags
-    command += " -o " + out_file
-    # Execute command
-
-    print(command)
-    print("Going to execute: " + command)
-    output = subprocess.check_output(command, shell=True)
-    print("Done:")
-    print(in_file + " > " + out_file)
-    os.remove(fileToConvert)
 #volume = ConvexHull(tree).volume
-print("# Projected tumor size (max): \n## Major axis:" +
-      str(major_axis) + " \n## Minor axis:" + str(minor_axis))
-print("# done. Happy 3D printing!")
+print("# Done. Happy 3D printing!")
 
 # print("# Upload to kidney.cyted.io")
 ftp = FTP('cyted.io')
-print ("Welcome: ", ftp.getwelcome())
-ftp.login("kidney@cyted.io", "#")
+print("Welcome: ", ftp.getwelcome())
+ftp.login("kidney@cyted.io", "puffintheprinter")
 ftp_upload(ftp, "Patient.stl", ftype="STL")
 ftp_upload(ftp, "Patient_tumour.stl", ftype="STL")
 ftp_upload(ftp, "Patient_kidney.stl", ftype="STL")
